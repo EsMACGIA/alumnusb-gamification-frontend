502935b9adf6e77e8858907f6262ac9250f144e3
Carlos Rivero <carlosrivero96@gmail.com>
2021-02-07 16:28:49 -0400

    Minor changes

--- src/app/pages/stats/stats.module.ts
+++ src/app/pages/stats/stats.module.ts
@@ -1,7 +1,5 @@
 import { NgModule } from '@angular/core';
-import {
-    NbCardModule,
-  } from '@nebular/theme';
+import { NbCardModule } from '@nebular/theme';
 
 import { ThemeModule } from '../../@theme/theme.module';
 import { StatsComponent } from './stats.component';
