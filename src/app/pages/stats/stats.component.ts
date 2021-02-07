2934ee6d21c8d6170be860835f754dc99408d6b8
Carlos Rivero <carlosrivero96@gmail.com>
2021-02-06 20:08:30 -0400

    Created user stats component

---
+++ src/app/pages/stats/stats.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+
+import { StatsModel } from './stats.model';
+
+
+@Component({
+  selector: 'stats-component',
+  templateUrl: './stats.component.html',
+})
+
+export class StatsComponent {
+
+stats = {
+    email: "jean@gmail.com",
+    average_gift: 0.0,
+    largest_gift: 0.0,
+    smallest_gift: 0.0,
+    total_gifts: 0.0,
+    best_gift_year_total: 0.0,
+    best_gift_year: 0,
+    first_gift_date: "2020-01-01",
+    last_gift_date: "2020-01-01",
+    total_number_of_gifts: 0
+    }
+
+};
---
+++ src/app/pages/stats/stats.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { 
+    NbCardModule 
+  } from '@nebular/theme';
+
+import { ThemeModule } from '../../@theme/theme.module';
+import { StatsComponent } from './stats.component';
+
+@NgModule({
+    
+    imports: [
+      NbCardModule,
+      ThemeModule,
+    ],
+    declarations: [
+      StatsComponent
+    ]
+})
+
+export class StatsModule {}
