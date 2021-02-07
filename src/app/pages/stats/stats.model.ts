276ecc1647b46c3267596423b32eba235bca7bae
Carlos Rivero <32308116+CarlosRivero96@users.noreply.github.com>
2021-02-07 16:34:59 -0400

    Created user stats component (#3)
    * Created user stats component
    
    * Added date and currency pipes
    
    * Cleaning code
    
    * Minor changes

---
+++ src/app/pages/stats/stats.model.ts
@@ -0,0 +1,17 @@
+/**
+ * @export
+ * @class StatsModel
+ */
+
+export class StatsModel {
+    email: string;
+    average_gift: number;
+    largest_gift: number;
+    smallest_gift: number;
+    total_gifts: number;
+    best_gift_year_total: number;
+    best_gift_year: number;
+    first_gift_date: string;
+    last_gift_date: string;
+    total_number_of_gifts: number;
+}
