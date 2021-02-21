a78a5e128f79d43528053d961f952650fa709aed
Wilfredo Graterol <solo.wilfredo64@gmail.com>
2021-02-20 23:19:57 -0400

    Updated css

--- src/app/pages/upload-csv/upload-csv.module.ts
+++ src/app/pages/upload-csv/upload-csv.module.ts
@@ -1,21 +1,17 @@
 import { NgModule } from '@angular/core';
-import { NbCardModule, NbListModule } from '@nebular/theme';
+import { NbCardModule, NbListModule, NbButtonModule } from '@nebular/theme';
 
 // Components
 import { UploadCsvComponent } from './upload-csv.component';
 
 import { ThemeModule } from '../../@theme/theme.module';
 
-// import { CollapseModule } from 'ngx-bootstrap/collapse';
-// import { RouterModule } from '@angular/router';
-
 @NgModule({
   imports: [
-    // RouterModule,
-    // CollapseModule.forRoot(),
     NbCardModule,
     ThemeModule,
     NbListModule,
+    NbButtonModule,
   ],
   declarations: [
     UploadCsvComponent,
