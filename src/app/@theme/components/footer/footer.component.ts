f42bc3c319a39224f713289e3f4a9dd54b66e04c
Jose <josedavidb6@gmail.com>
2021-02-20 22:01:56 -0400

    Dashboard footer

--- src/app/@theme/components/footer/footer.component.ts
+++ src/app/@theme/components/footer/footer.component.ts
@@ -5,13 +5,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.scss'],
   template: `
     <span class="created-by">
-      Created with ♥ by <b><a href="https://akveo.page.link/8V2f" target="_blank">Akveo</a></b> 2019
+      Creado con ♥ por <b>MACGIA</b> 2021
     </span>
     <div class="socials">
-      <a href="#" target="_blank" class="ion ion-social-github"></a>
-      <a href="#" target="_blank" class="ion ion-social-facebook"></a>
-      <a href="#" target="_blank" class="ion ion-social-twitter"></a>
-      <a href="#" target="_blank" class="ion ion-social-linkedin"></a>
+      <a
+        class="btn btn-icon btn-neutral btn-round btn-simple mr-1"
+        href="https://www.instagram.com/alumn_usb/"
+        target="_blank"
+      >
+        <i class="fab fa-instagram"> </i>
+      </a>
+      <a
+        class="btn btn-icon btn-neutral btn-round btn-simple mr-1"
+        href="https://twitter.com/alumn_usb/"
+        target="_blank"
+      >
+        <i class="fab fa-twitter"> </i>
+      </a>
+      <a
+        class="btn btn-icon btn-neutral btn-round btn-simple mr-1"
+        href="https://www.facebook.com/alumnusb/"
+        target="_blank"
+      >
+        <i class="fab fa-facebook"> </i>
+      </a>
+      <a
+        class="btn btn-icon btn-neutral btn-round btn-simple mr-1"
+        href="https://www.youtube.com/c/alumnusb/"
+        target="_blank"
+      >
+        <i class="fab fa-youtube"> </i>
+      </a>
+      <a
+        class="btn btn-icon btn-neutral btn-round btn-simple"
+        href="https://www.linkedin.com/company/alumnusb/"
+        target="_blank"
+      >
+        <i class="fab fa-linkedin"> </i>
+      </a>
     </div>
   `,
 })
