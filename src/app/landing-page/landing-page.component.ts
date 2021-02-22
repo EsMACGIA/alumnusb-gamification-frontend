6f09478a55892e399eb50bfa40c489a5e374ee10
juan <jaoc1811@gmail.com>
2021-02-22 17:12:46 -0400

    'Cargar CSV' button shows only when you are admin

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -30,6 +30,7 @@ export class LandingPageComponent implements OnInit, OnDestroy {
   isCollapsed = true;
   private alive = true;
   isAuthenticated: boolean = false;
+  isAdmin: boolean = false;
   user: any;
 
   constructor(
@@ -60,6 +61,7 @@ export class LandingPageComponent implements OnInit, OnDestroy {
     body.classList.add('landing-page');
     this.user = this.authService.getUserInfo();
     this.isAuthenticated = this.authService.isAuthenticated();
+    this.isAdmin = (this.user.isAdmin == 'true')
   }
   ngOnDestroy() {
     this.alive = false;
