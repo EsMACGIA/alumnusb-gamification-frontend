0b78766477f5d7ef2664ab65e3935d44eaadb1c9
Manuel Faria <mfaria724@gmail.com>
2021-02-22 19:53:34 +0000

    Linting changes.

--- deploy/server.js
+++ deploy/server.js
@@ -5,8 +5,8 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static('dist/'));
 
-app.get('/*', function(req,res) {  
-  res.sendFile('index.html', { root: 'dist/'}  );
+app.get('/*', (req, res) => {  
+  res.sendFile('index.html', { root: 'dist/' });
 });
 
 let port = process.env.PORT || 8080
