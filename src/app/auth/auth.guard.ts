f0ee8ce1bbc831c9508fbf85220e738c4b6aa389
Juan Oropeza <33599807+jaoc1811@users.noreply.github.com>
2021-03-16 15:33:11 -0400

    Redirect from upload-csv to homepage when user is not admin (#38)

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -19,6 +19,10 @@ export class AuthGuard implements CanActivate {
         return false;
       }
 
+    if (route.data.admin !== this.authService.isAdmin()) {
+      this.router.navigate(['/']);
+      return false;
+    }
       // Authorised so return true
       return true;
     }
