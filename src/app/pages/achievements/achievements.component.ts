71609786f22816c11e5c5e7bc8db30e39ff31e2f
Carlos Rivero <32308116+CarlosRivero96@users.noreply.github.com>
2021-03-18 16:55:56 -0400

    Added toaster for errors (#43)

--- src/app/pages/achievements/achievements.component.ts
+++ src/app/pages/achievements/achievements.component.ts
@@ -4,12 +4,21 @@ import { AchievementsModel } from './achievements.model';
 import { AchievementsService } from './achievements.service';
 import { medals } from '../../../assets/data/medals';
 
+import { NbToastrService, NbComponentStatus, NbGlobalPosition, NbGlobalPhysicalPosition} from '@nebular/theme';
+
 @Component({
   selector: 'ngx-achievements',
   templateUrl: './achievements.component.html',
   styleUrls: ['./achievements.component.scss'],
 })
 export class AchievementsComponent implements OnInit {
+  destroyByClick = false;
+  duration = 4000;
+  hasIcon = true;
+  preventDuplicates = false;
+  position: NbGlobalPosition = NbGlobalPhysicalPosition.TOP_RIGHT;
+  index = 1;
+
   userId: Number;
   loading = false;
   achievements = new AchievementsModel();
@@ -18,8 +27,8 @@ export class AchievementsComponent implements OnInit {
   constructor(
     private achievementsService: AchievementsService,
     private authService: AuthService,
-  ) {
-  }
+    private toastrService: NbToastrService,
+  ) {}
 
   ngOnInit() {
     this.loadAchievements();
@@ -29,11 +38,39 @@ export class AchievementsComponent implements OnInit {
   loadAchievements() {
     this.loading = true;
     this.userId = this.authService.userId;
-    this.achievementsService.getAchievements(this.userId).subscribe(data => {
-      if (data) {
-        this.achievements = data;
-      }
-      this.loading = false;
-    });
+    this.achievementsService.getAchievements(this.userId).subscribe(
+      data => {
+        if (data) {
+          if (data.achieved) {
+            this.achievements = data;
+          }
+        } else if (data.error) {
+          for (const error of data.error.error) {
+            this.showToast('danger', error, '');
+          }
+        }
+        this.loading = false;
+      },
+      err => {
+        this.showToast('danger', 'Hubo un error, intente de nuevo', '');
+      },
+    );
+  }
+
+  private showToast(type: NbComponentStatus, title: string, body: string) {
+    const config = {
+      status: type,
+      destroyByClick: this.destroyByClick,
+      duration: this.duration,
+      hasIcon: this.hasIcon,
+      position: this.position,
+      preventDuplicates: this.preventDuplicates,
+    };
+
+    this.index += 1;
+    this.toastrService.show(
+      body,
+      title,
+      config);
   }
 }
--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -4,6 +4,7 @@ import { NbDialogService } from '@nebular/theme';
 import { ProfileModel } from './profile.model';
 import { ProfileService } from './profile.service';
 import { ProfileFormComponent } from './profile-form/profile-form.component';
+import { NbToastrService, NbComponentStatus, NbGlobalPosition, NbGlobalPhysicalPosition} from '@nebular/theme';
 
 @Component({
   selector: 'ngx-profile',
@@ -12,13 +13,22 @@ import { ProfileFormComponent } from './profile-form/profile-form.component';
 })
 export class ProfileComponent implements OnInit {
 
+  destroyByClick = false;
+  duration = 4000;
+  hasIcon = true;
+  preventDuplicates = false;
+  position: NbGlobalPosition = NbGlobalPhysicalPosition.TOP_RIGHT;
+  index = 1;
+
   profileData: ProfileModel = new ProfileModel();
   id: number;
   loading = false;
 
   constructor(
     private nbDialogService: NbDialogService,
-    private profileService: ProfileService) {}
+    private profileService: ProfileService,
+    private toastrService: NbToastrService,
+    ) {}
 
   ngOnInit() {
     this.id = Number(localStorage.getItem('userId'));
@@ -53,17 +63,45 @@ export class ProfileComponent implements OnInit {
             }
           });
   }
+
   open() {
     this.nbDialogService.open(ProfileFormComponent, { closeOnBackdropClick: false , hasScroll: true, context: {profileData: this.profileData}})
     .onClose.subscribe(profile => {
       if (profile) {
-        this.profileService.updateProfile(profile, this.id).subscribe(data => {
-          if (data) {
-            this.getProfileData(this.id);
-          }
-        });
+        this.profileService.updateProfile(profile, this.id).subscribe(
+          data => {
+            if (data) {
+              if (data.first_name) {
+                this.getProfileData(this.id);
+              } else if (data.error) {
+                for (const error of data.error.error) {
+                  this.showToast('danger', error, '');
+                }
+              }
+            }
+          },
+          err => {
+            this.showToast('danger', 'Hubo un error, intente de nuevo', '');
+          },
+        );
       }
     });
   }
 
+  private showToast(type: NbComponentStatus, title: string, body: string) {
+    const config = {
+      status: type,
+      destroyByClick: this.destroyByClick,
+      duration: this.duration,
+      hasIcon: this.hasIcon,
+      position: this.position,
+      preventDuplicates: this.preventDuplicates,
+    };
+
+    this.index += 1;
+    this.toastrService.show(
+      body,
+      title,
+      config);
+  }
 }
